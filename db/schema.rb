84f35bcf18e438bf2f8c4fc612dad0545b6cf1cc
Jonathan Serafini <jonathan.serafini@gmail.com>
2017-06-23 14:42:25 +0200

    migration add columns to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170623123011) do
+ActiveRecord::Schema.define(version: 20170623123012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,18 @@ ActiveRecord::Schema.define(version: 20170623123011) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "city"
+    t.string "country"
+    t.integer "gender"
+    t.text "bio"
+    t.string "book"
+    t.string "music"
+    t.string "movie"
+    t.datetime "birthday"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
