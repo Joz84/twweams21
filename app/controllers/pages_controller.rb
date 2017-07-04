0a60f844dd00873341ab853d16305cc5c8f4047e
Jonathan Serafini <jonathan.serafini@gmail.com>
2017-07-04 14:35:15 +0200

    Update pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,11 +3,7 @@ class PagesController < ApplicationController
   def home
     @users = User.all
 
-    @users.each do |user|
-      @user_coordinates = { lat: user.latitude, lng: user.longitude }
-    end
-
-    @hash = Gmaps4rails.build_markers([@user]) do |user, marker|
+    @hash = Gmaps4rails.build_markers(@users) do |user, marker|
       marker.lat user.latitude
       marker.lng user.longitude
       # marker.infowindow render_to_string(partial: "/flats/map_box", locals: { flat: flat })
