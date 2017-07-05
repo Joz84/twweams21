190cf3d69475b206056a0f6da7dad9ca6d6d333f
mharribey <mathieuharribey@hotmail.fr>
2017-07-05 12:01:07 +0200

    try to fix marker display

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,11 +6,9 @@ class PagesController < ApplicationController
     @hash = Gmaps4rails.build_markers(@users) do |user, marker|
       marker.lat user.latitude
       marker.lng user.longitude
-      marker.picture ({
-          :url => view_context.image_path(@gender_icon = (user.gender == 1 ? 'elements/Point-bleu-01.png' : 'elements/Point-rose-01.png')),
-          :width => 16,
-          :height => 16
-        })
+      marker.picture({
+                  url: view_context.image_path(@gender_icon = (user.gender == 1 ? 'elements/Point-bleu-01.png' : 'elements/Point-rose-01.png')),
+                 })
       # marker.infowindow render_to_string(partial: "/flats/map_box", locals: { flat: flat })
     end
   end
