ca69e5bc5dab7bbc343ebc47c1f8ff2b92307ad8
mharribey <mathieuharribey@hotmail.fr>
2017-07-05 14:12:29 +0200

    add custom markers to map

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,8 +7,11 @@ class PagesController < ApplicationController
       marker.lat user.latitude
       marker.lng user.longitude
       marker.picture({
-                  url: view_context.image_path(@gender_icon = (user.gender == 1 ? 'elements/Point-bleu-01.png' : 'elements/Point-rose-01.png')),
-                 })
+          "url": "#{view_context.image_path(@gender_icon = (user.gender == 1 ? 'elements/point-bleu-01.png' : 'elements/point-rose-01.png'))}",
+          "width":  24,
+          "height": 45
+        })
+      # marker.picture view_context.image_path(@gender_icon = (user.gender == 1 ? 'elements/point-bleu-01.png' : 'elements/point-rose-01.png'))
       # marker.infowindow render_to_string(partial: "/flats/map_box", locals: { flat: flat })
     end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,11 @@ class UsersController < ApplicationController
     @hash = Gmaps4rails.build_markers([@user]) do |user, marker|
       marker.lat user.latitude
       marker.lng user.longitude
+      marker.picture({
+          "url": "#{view_context.image_path(@gender_icon = (user.gender == 1 ? 'elements/point-bleu-01.png' : 'elements/point-rose-01.png'))}",
+          "width":  24,
+          "height": 45
+        })
       # marker.infowindow render_to_string(partial: "/flats/map_box", locals: { flat: flat })
     end
   end
