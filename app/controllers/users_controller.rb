047c3bff9ac997373c690404dc0ae1f0839f83da
mharribey <mathieuharribey@hotmail.fr>
2017-07-10 12:54:59 +0200

    update user#edit front

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
       marker.lat user.latitude
       marker.lng user.longitude
       marker.picture({
-          "url": "#{view_context.image_path(@gender_icon = (user.gender == 'male' ? 'elements/point-bleu-01.png' : 'elements/point-rose-01.png'))}",
+          "url": "#{view_context.image_path(@gender_icon = (user.gender == 'male' ? 'elements/Point-bleu-01.png' : 'elements/Point-rose-01.png'))}",
           "width":  24,
           "height": 45
         })
@@ -26,11 +26,25 @@ class UsersController < ApplicationController
       marker.lat user.latitude
       marker.lng user.longitude
       marker.picture({
-          "url": "#{view_context.image_path(@gender_icon = (user.gender == 'male' ? 'elements/point-bleu-01.png' : 'elements/point-rose-01.png'))}",
+          "url": "#{view_context.image_path(@gender_icon = (user.gender == 'male' ? 'elements/Point-bleu-01.png' : 'elements/Point-rose-01.png'))}",
           "width":  24,
           "height": 45
         })
       # marker.infowindow render_to_string(partial: "/flats/map_box", locals: { flat: flat })
     end
   end
+
+  def edit
+      @user = current_user
+      @hash = Gmaps4rails.build_markers([@user]) do |user, marker|
+        marker.lat user.latitude
+        marker.lng user.longitude
+        marker.picture({
+            "url": "#{view_context.image_path(@gender_icon = (user.gender == 'male' ? 'elements/Point-bleu-01.png' : 'elements/Point-rose-01.png'))}",
+            "width":  24,
+            "height": 45
+          })
+      end
+  end
+
 end
