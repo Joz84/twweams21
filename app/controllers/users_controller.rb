9416898c240f56b6011db6d911bf861a3c8c2c08
mharribey <mathieuharribey@hotmail.fr>
2017-07-12 10:40:29 +0200

    show map in registrations#edit + update front in registrations#new and sessions#new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,18 +33,4 @@ class UsersController < ApplicationController
       # marker.infowindow render_to_string(partial: "/flats/map_box", locals: { flat: flat })
     end
   end
-
-  def edit
-      @user = current_user
-      @hash = Gmaps4rails.build_markers([@user]) do |user, marker|
-        marker.lat user.latitude
-        marker.lng user.longitude
-        marker.picture({
-            "url": "#{view_context.image_path(@gender_icon = (user.gender == 'male' ? 'elements/Point-bleu-01.png' : 'elements/Point-rose-01.png'))}",
-            "width":  24,
-            "height": 45
-          })
-      end
-  end
-
 end
